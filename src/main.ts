83c70ac2eb11d440024aad0eac9596d785a79256
Prince Anumba <princeanumba0@gmail.com>
2025-11-27 12:49:03 +0000

    added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    // forbidNonWhitelisted: true,
-    // transform: true,
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // forbidNonWhitelisted: true,
+      // transform: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API Description')
+    .setVersion('1.0')
+    .addTag('api') // Optional: Add tags to group endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // 'api-docs' is the path where Swagger UI will be accessible
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
